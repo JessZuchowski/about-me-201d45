b5b2eeeb7835c36413f813ef16cf0b1be99c07b8
jaezuchowski <jess.zuchowski@gmail.com>
2019-02-08 03:13:44 -0800

    my second commit

--- js/app.js
+++ js/app.js
@@ -1,40 +1,43 @@
+'use strict';
+
 var coding, games, fiction, hiking, collecting;
 
-coding = prompt("Do you like coding?");
-if (coding == "yes") {
-  alert("I am just learning coding and am having fun.");
-} else {
-  alert("Coding takes a lot of effort to learn.");
-}
-console.log("Asked about coding, result : " + coding);
 
-games = prompt("Do you like playing video games?");
-if (games == "yes") {
-  alert("I like playing fantasy RPGs.");
+coding = prompt('Do you like coding?');
+if (coding === 'yes') {
+  alert('I am just learning coding and am having fun.');
 } else {
-  alert("I hope you enjoy other types of games, then.");
+  alert('Coding takes a lot of effort to learn.');
 }
-console.log("Asked about games, result : " + games);
+console.log('Asked about coding, result : ' + coding);
 
-fiction = prompt("Do you like reading fiction?");
-if (fiction == "yes") {
-  alert("I read more fiction than anything else.");
+games = prompt('Do you like playing video games?');
+if (games === 'yes') {
+  alert('I like playing fantasy RPGs.');
 } else {
-  alert("I try to read other things too, like HTML tutorials.");
+  alert('I hope you enjoy other types of games, then.');
 }
-console.log("Asked about fiction, result : " + fiction);
+console.log('Asked about games, result : ' + games);
 
-hiking = prompt("Do you like hiking in the woods?");
-if (hiking == "yes") {
-  alert("Hiking is one of my favorite things to do.");
+fiction = prompt('Do you like reading fiction?');
+if (fiction === 'yes') {
+  alert('I read more sci-fi and fantasy than anything else.');
 } else {
-  alert("You might enjoy getting out more.");
+  alert('I try to read other things too, like JS tutorials.');
 }
-console.log("Asked about hiking, result : " + hiking);
+console.log('Asked about fiction, result : ' + fiction);
 
-collecting = prompt("Do you like collecting things?");
-if (collecting == "yes") {
-  alert("I have a collection of insects.");
+hiking = prompt('Do you like hiking in the woods?');
+if (hiking === 'yes') {
+  alert('I like looking for mushrooms while out hiking.');
 } else {
-  alert("I probably have too many collections.");
+  alert('You might enjoy getting out more.');
+}
+console.log('Asked about hiking, result : ' + hiking);
+
+collecting = prompt('Do you like collecting things?');
+if (collecting === 'yes') {
+  alert('I have a collection of insects.');
+} else {
+  alert('I might have too many collections, do I really need to save acorns?');
 }
