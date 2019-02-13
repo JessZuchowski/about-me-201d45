8f1716716cf865763e56babb1ed9a3f93b5c10f8
jaezuchowski <jess.zuchowski@gmail.com>
2019-02-12 20:27:31 -0800

    my third commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # about-me-201d45
+Welcome to this little About Me page. Here you will find some fun and professional facts about me, along with a few questions and guessing games to interact with as a way of telling even more about myself.
+
+The code here has been built using the Code Fellows 201d45 class lectures and demos. I have also utilized Jon Duckett's book Javascript & jQuery, particularly chapter 4. MDN was a useful resource in adding the .toLowerCase() method to allow for more input options for the yes/no questions.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+
+I was having some issues with the format of Question 6, particularly in getting the console.logs to work even though to me it looked like they should have worked the same as in the previous questions. After watching code review in class of Aliya's project and seeing that the format was similar to mine, I made appropriate adjustments based on Nicholas' feeback. 
+
+I wanted my answers for the guessing game to be visible to the viewer after completing the prompts. I knew I had, years ago, created hovering text with some simple HTML edits but couldn't remember how to do it. I found a solution here:
+http://sites.cognitivescience.co/knowledgebase/resources/using-google-sites/creating-mouseover-text-with-html
+and also changed text color within that tag to match my CSS list items.
--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@
 
 var guest, coding, games, fiction, hiking, collecting;
 
+// Intro Question - Name
 guest = prompt('What is your name?');
 alert('Hi ' + guest +', it is lovely to meet you. My name is Jess.');
 console.log('Asked for a guest name, result : ' + guest);
 //Asked for a guest name, returned name in alert, logged answer.
 
+
+// Question 1 - yes/y/other(no)
 coding = prompt('Do you like coding?');
 if (coding.toLowerCase() === 'yes') {
   alert('I am just learning coding and am having fun.');
@@ -17,6 +20,8 @@ if (coding.toLowerCase() === 'yes') {
 console.log('Asked about coding, result : ' + coding);
 //Asked about coding, return different alerts for 'yes' 'y' or other answers. Yes/Y adjust to lower case. Logs answer.
 
+
+// Question 2 - yes/y/other(no)
 games = prompt('Do you like playing video games?');
 if (games.toLowerCase() === 'yes') {
   alert('Me too, especially fantasy RPGs.');
@@ -28,6 +33,8 @@ if (games.toLowerCase() === 'yes') {
 console.log('Asked about games, result : ' + games);
 //Asked about video games, return different alerts for 'yes' 'y' or other answers. Yes/Y adjust to lower case. Logs answer.
 
+
+// Question 3 - yes/y/other(no)
 fiction = prompt('Do you like reading fiction?');
 if (fiction.toLowerCase() === 'yes') {
   alert('I read more sci-fi and fantasy than anything else.');
@@ -39,6 +46,8 @@ if (fiction.toLowerCase() === 'yes') {
 console.log('Asked about fiction, result : ' + fiction);
 //Asked about reading fiction, return different alerts for 'yes' 'y' or other answers. Yes/Y adjust to lower case. Logs answer.
 
+
+// Question 4 - yes/y/other(no)
 hiking = prompt('Do you like hiking in the woods?');
 if (hiking.toLowerCase() === 'yes') {
   alert('I like looking for mushrooms while out hiking.');
@@ -50,8 +59,10 @@ if (hiking.toLowerCase() === 'yes') {
 console.log('Asked about hiking, result : ' + hiking);
 //Asked about hiking, return different alerts for 'yes' 'y' or other answers. Yes/Y adjust to lower case. Logs answer.
 
+
+// Question 5 - yes/y/other(no)
 collecting = prompt('Do you like collecting things?');
-if (collecting.toLowerCase === 'yes') {
+if (collecting.toLowerCase() === 'yes') {
   alert('I have a few collections including succulents, insects, and small wooden chests.');
 } else if (collecting.toLowerCase() === 'y') {
   alert('I am happy with the art I have collected over the years.');
@@ -60,3 +71,50 @@ if (collecting.toLowerCase === 'yes') {
 }
 console.log('Asked about collections, result : ' + collecting);
 //Asked about collections, return different alerts for 'yes' 'y' or other answers. Yes/Y adjust to lower case. Logs answer.
+
+
+// Question 6 - Number Guessing 
+var countriesNumber = 8;
+var countriesGuess = prompt('Can you guess the number of countries I have been to? Pick from 1-10.');
+for (var guess = 3; guess > 0; guess--){
+  if (countriesGuess > countriesNumber) {
+    console.log('Asked to guess a number of countries, result : ' + countriesGuess , 'was too big');
+    countriesGuess = prompt ('' + countriesGuess + ' is too big. Guess again.');   
+  } else if (countriesGuess < countriesNumber) {
+    console.log('Asked to guess a number of countries, result : ' + countriesGuess , 'was too small');
+    countriesGuess = prompt ('' + countriesGuess + ' is too small. Guess again.');
+  } else if (countriesGuess == countriesNumber) {
+    console.log('Asked to guess a number of countries, result : ' + countriesGuess , 'was correct');
+    countriesGuess = alert ('That is correct! I hope to visit more some day.');
+    break;
+  } else {
+    countriesGuess = prompt ('Please supply a numeric value.');
+  }
+}
+// What I played around with: I tried using alerts instead of prompts at first and that did not run through the script. Changing to prompts allowed script to run. I initailly wrote console.logs after the prompts but that did not yield intended results, so I went back and watched some code review. Switching logs with prompts worked. Had to remember to keep correct guess as an alert so that guessing prompts would stop there. Having === for correct guess did not yield intended results, changing to == worked but I'm not sure if it's the best fix. 
+// What I ended up with: Asked to guess a number between 1-10 corresponding to countries been to. Gave a total of 4 guesses. Set guesses to 3 with a countdown using --. Logs each answer given with how it relates to correct number.        
+
+
+// Question 7 - Guessing with Multiple Correct Answers
+var creativeOutlets = ['cooking', 'sewing', 'drawing'];
+var outletGuess = prompt('Can you guess one of my favorite creative outlets?');
+for (var guesses = 5; guesses > 0; guesses--){
+  if (outletGuess.toLowerCase() === creativeOutlets[0]) {
+    console.log('Asked to guess a creative outlet, result : ' + outletGuess , 'was a correct answer.');
+    outletGuess = alert ('I cook almost every day and have the most fun baking cookies!');
+    break;
+  } else if (outletGuess.toLowerCase() === creativeOutlets[1]) {
+    console.log('Asked to guess a creative outlet, result : ' + outletGuess , 'was a correct answer.');
+    outletGuess = alert ('I like to sew small things like dice bags.');
+    break;
+  } else if (outletGuess.toLowerCase() === creativeOutlets[2]) {
+    console.log('Asked to guess a creative outlet, result : ' + outletGuess , 'was a correct answer.');
+    outletGuess = alert ('I usually draw in Photoshop, mostly fantasy character portraits.');
+    break;
+  } else {
+    outletGuess = prompt ('' + outletGuess +' sounds fun, but try guessing something like woodworking or coding.');
+    console.log('Asked to guess a creative outlet, result : ' + outletGuess , 'was incorrect.');
+  }
+}
+// Uses an array for three possible correct answers. Guesses are set to 5 inside of the for loop, giving a total of 6. Each correct answer gives a different alert with more information about my favorite creative outlet, then breaks out of the loop. An incorrect guess give a prompt that reads back the guess as well as hints at the format I'm looking for. Console.logs are kept for all answers. 
+
